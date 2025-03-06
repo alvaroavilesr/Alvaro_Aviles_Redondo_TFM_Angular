6567b7311add7a560926f11cb65738247b9b90dc
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-03-06 16:08:18 +0100

    Added home page #2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {HelpComponentComponent} from './help-component/help-component.component';
+import {AboutComponent} from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'help', component: HelpComponentComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo:'home', pathMatch: 'full' },
+  { path: '**', redirectTo:'home' }
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,48 +14,5 @@ import { Location } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements AfterViewInit, OnDestroy {
-
-  constructor(private router: Router,
-              private location: Location,
-              private cdr: ChangeDetectorRef) {}
-
-  protected readonly sessionStorage = sessionStorage;
-  isLoggedIn = false;
-
-  ngAfterViewInit() {
-    if (localStorage.getItem('logoutOngoing') === 'true') {
-      localStorage.removeItem('logoutOngoing');
-      localStorage.setItem('logoutSuccessful', 'true');
-      this.router.navigate(['/home']);
-    }else{
-      this.checkLoginStatus();
-      window.addEventListener('storage', this.handleStorageChange.bind(this));
-    }
-  }
-
-  ngOnDestroy() {
-    window.removeEventListener('storage', this.handleStorageChange.bind(this));
-  }
-
-  private checkLoginStatus() {
-    this.isLoggedIn = !!sessionStorage.getItem('token');
-    this.cdr.detectChanges();
-  }
-
-  private handleStorageChange(event: StorageEvent) {
-    if (event.key === 'token') {
-      this.checkLoginStatus();
-    }
-  }
-
-  logout() {
-    localStorage.setItem('logoutOngoing', 'true');
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('username');
-    this.checkLoginStatus();
-    this.location.go(this.location.path());
-    window.location.reload();
-  }
-
+export class NavbarComponent {
 }
