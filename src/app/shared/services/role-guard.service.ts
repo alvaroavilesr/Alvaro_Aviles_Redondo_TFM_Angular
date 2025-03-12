c2d647eccfca918f9322fe5a608cc4f880218b27
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-03-12 13:27:53 +0100

    Added role-guard service #4

---
+++ src/app/shared/services/role-guard.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { Roles } from '../roles';
+
+@Injectable({ providedIn: 'root' })
+export class RoleGuardService implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const token = sessionStorage.getItem('JWT');
+    const userRole = sessionStorage.getItem('Role');
+    const requiredRoles: Roles[] = route.data['roles'];
+
+    if (!token) {
+      this.router.navigate(['/home']);
+      console.log('No token');
+      return false;
+    }
+
+    if (requiredRoles && requiredRoles.length > 0 && !requiredRoles.includes(userRole as Roles)) {
+      this.router.navigate(['/home']);
+      console.log('No role');
+      return false;
+    }
+
+    return true;
+  }
+}
