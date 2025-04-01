7011fc60ff857a8f9de7833842eaf3e34626856f
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-04-01 12:13:38 +0200

    Created profile info panel #5

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
 
+export class ProfileComponent implements OnInit {
+  userData: any;
+
+  ngOnInit(): void {
+    this.userData = {
+      username: sessionStorage.getItem('UserName'),
+      email: sessionStorage.getItem('UserEmail'),
+      name: sessionStorage.getItem('FirstName'),
+      surname: sessionStorage.getItem('LastName')
+    };
+  }
+
+  showAlert(): void {
+    alert('Button clicked!');
+  }
 }
