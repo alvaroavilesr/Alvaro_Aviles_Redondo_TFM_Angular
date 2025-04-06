b42c12b178e8da2987baddf12c3e697c433c38b4
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-04-06 23:11:34 +0200

    Added intial template #9

--- src/app/vendor/item-management/item-management.component.ts
+++ src/app/vendor/item-management/item-management.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-item-management',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './item-management.component.html',
   styleUrl: './item-management.component.css'
 })
 export class ItemManagementComponent {
-
+  filteredProducts: any[] = [];
+  products: any[] = [];
+  searchTerm: string = '';
+  selectedCategory: string = 'Any';
 }
