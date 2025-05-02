0c9a31cc03cb4d5fb9ff9497475330ef47d8fc99
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-05-02 12:19:34 +0200

    Fixed test #10

--- src/app/vendor/order-management/order-management.component.spec.ts
+++ src/app/vendor/order-management/order-management.component.spec.ts
@@ -1,20 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderManagementComponent } from './order-management.component';
+import {ToastrService} from 'ngx-toastr';
+import {OrderManagementService} from './order-management.service';
 
 describe('OrderManagementComponent', () => {
   let component: OrderManagementComponent;
   let fixture: ComponentFixture<OrderManagementComponent>;
+  let orderManagementService: jasmine.SpyObj<OrderManagementService>;
+  let toastrService: jasmine.SpyObj<ToastrService>;
 
   beforeEach(async () => {
+    const orderManagementSpy = jasmine.createSpyObj('OrderManagementService', [
+      'getOrders',
+      'deleteOrder'
+    ]);
+    const toastrSpy = jasmine.createSpyObj('ToastrService', ['success', 'error']);
     await TestBed.configureTestingModule({
-      imports: [OrderManagementComponent]
+      imports: [OrderManagementComponent],
+      providers: [
+        { provide: OrderManagementService, useValue: orderManagementSpy },
+        { provide: ToastrService, useValue: toastrSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(OrderManagementComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    orderManagementService = TestBed.inject(OrderManagementService) as jasmine.SpyObj<OrderManagementService>;
+    toastrService = TestBed.inject(ToastrService) as jasmine.SpyObj<ToastrService>;
   });
 
   it('should create', () => {
